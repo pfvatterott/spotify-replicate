c17e53c1bec0999f17c98b0bfa578f2e3770d4a1
Paul Vatterott <pfvatterott@gmail.com>
2023-10-07 15:49:39 -0600

    replicate api

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -5,6 +5,7 @@ import axios from "axios"
 function Profile() {
     const location = useLocation();
     const [profileData, setProfileData] = useState(false);
+    const [input, setInput] = useState("");
     useEffect(() => {
         axios.post('/api/spotify/getProfileData', {
             bearerToken: location.state.bearerToken,
@@ -24,14 +25,15 @@ function Profile() {
         if (e.key === "Enter") {
             axios.post('/api/spotify/getTopArtists', {
                 bearerToken: location.state.bearerToken,
-                refreshToken: location.state.refreshToken
+                refreshToken: location.state.refreshToken,
+
             })
                 .then(function (response) {
                     console.log(response.data)
                     let favoriteArtistArray = response.data.items
                     let aiResponse = axios.post('/api/replicate/query', {
                         favoriteArtists: favoriteArtistArray,
-                        prompt: 'promptFiller'
+                        prompt: input
                     })
                     console.log(aiResponse)
 
@@ -51,7 +53,7 @@ function Profile() {
                 <h2>Name: {profileData.display_name}</h2>
             </div>
             <div className="flex flex-row justify-center">
-            <input type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your request here" onKeyPress={handleKeyPress}></input>
+            <input type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your request here" onKeyUp={handleKeyPress} onInput={e => setInput(e.target.value)}></input>
             </div>
         </div>
     );
--- routes/api/replicate.js
+++ routes/api/replicate.js
@@ -9,8 +9,15 @@ const queryModel = async (req, res) => {
             auth: process.env.REPLICATE_API_TOKEN,
         });
 
+        let favoriteArtists = req.body.favoriteArtists
+        let newFavArtistsArray = []
+        for (let i = 0; i < favoriteArtists.length; i++) {
+            newFavArtistsArray.push(favoriteArtists[i].name)
+        }
+        const sysPrompt = `You will receive a prompt requesting you to make a playlist for the user. Provided with the prompt is an array of their favorite artists. Respond only with an array of objects. Each object should have an artist key value pair as well as a title key value pair.`
+
         const model = "meta/llama-2-70b-chat:02e509c789964a7ea8736978a43525956ef40397be9033abf9fd2badfe68c9e3";
-        const input = { prompt: "tell me a joke" };
+        const input = { prompt: `User prompt: ${req.body.prompt}. User's Favorite Artists: ${newFavArtistsArray}`, system_prompt: sysPrompt };
         const output = await replicate.run(model, { input });
 
         console.log(output.join(""));
