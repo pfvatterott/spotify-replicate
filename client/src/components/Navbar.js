1c1d2c7fcd85aaed0558366ce0c011dc27430941
Paul Vatterott <pfvatterott@gmail.com>
2023-10-10 20:42:46 -0600

    making things look prettier

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,13 +1,13 @@
 import React from "react";
 import logo from "./logo.png"
 
-function Navbar() {
-
+function Navbar(props) {
+console.log(props)
     return (
         <nav className="bg-white border-gray-200 dark:bg-gray-900">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <a href="http://localhost:3000/" className="flex items-center">
-                    <img src={logo} className="h-8 mr-3" alt="Paul Vatterott Logo" />
+                    <img src={props.props ? props.props : logo} className="h-8 mr-3 rounded-full" alt="Paul Vatterott Logo" />
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Spotify / Replicate AI</span>
                 </a>
                 <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
